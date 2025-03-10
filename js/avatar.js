1a3264e58eedc01e56a056bc4d378c32c126b6fe
Konstantin-Mazykin <konstantin.mazykin@gmail.com>
2025-03-10 22:45:50 +0200

    Fix avatar download function

--- js/avatar.js
+++ js/avatar.js
@@ -5,9 +5,9 @@ const viewImage = formRegistration.querySelector('.form-user-avatar');
 avatarInput.addEventListener('change', saveAvatar);
 
 function saveAvatar() {
-    let f = this.files[0];
-    if (f) {
-        viewImage.src = URL.createObjectURL(f);
+    let file = this.files[0];
+    if (file) {
+        viewImage.src = URL.createObjectURL(file);
         localStorage.setItem('myImage', viewImage.src);
     }
     viewImage.src = localStorage.getItem('myImage')
